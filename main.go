e9fc81f6c7655a8302a51e0f1af9f794efffa726
Ivan Melnyk <t3nna1001@gmail.com>
2025-11-24 12:53:48 +0100

    feat: read concurrently

--- main.go
+++ main.go
@@ -8,42 +8,52 @@ import (
 	"os"
 )
 
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	ch := make(chan string)
+
+	go func() {
+		defer f.Close()
+		defer close(ch)
+		fullLine := ""
+		for {
+			data := make([]byte, 8)
+			n, err := f.Read(data)
+			if err != nil {
+				break
+			}
+			data = data[:n]
+
+			if index := bytes.IndexByte(data, '\n'); index != -1 {
+				fullLine += string(data[:index])
+				ch <- fullLine
+				fullLine = ""
+				data = data[index+1:]
+			}
+			fullLine += string(data)
+
+		}
+		if fullLine != "" {
+			ch <- fullLine
+		}
+
+	}()
+	return ch
+}
+
 func main() {
 	file, err := os.Open("./messages.txt")
 	if err != nil {
 		log.Fatalf("unable to read a file %v", err)
 	}
-	defer file.Close()
 
-	line := ""
-
-	for {
-		data := make([]byte, 8)
-		n, err := file.Read(data)
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			fmt.Println(err)
-			continue
-		}
-		data = data[:n]
-
-		if i := bytes.IndexByte(data, '\n'); i != -1 {
-			line += string(data[:i])
-
-			data = data[i+1:]
-			fmt.Printf("read: %s \n", line)
-
-			line = ""
-		}
-		line += string(data)
-
+	ch := getLinesChannel(file)
+	for val := range ch {
+		fmt.Printf("read: %s \n", val)
 	}
 
 	// leftover
-	if len(line) != 0 {
-		fmt.Printf("read: %s \n", line)
-	}
+	//if len(line) != 0 {
+	//	fmt.Printf("read: %s \n", line)
+	//}
 
 }
